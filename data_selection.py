d2d5cf76e6925e6c524fadb05e6e7424fb82a1a2
rgb <ruben.gbarriada@gmail.com>
2021-02-17 20:36:59 +0100

    Preliminary vgg version

---
+++ data_selection.py
@@ -0,0 +1,79 @@
+import logging
+import os
+import shutil
+from distutils.dir_util import copy_tree
+
+OUTPUT_MAS = 'CACSmas400'
+OUTPUT_MENOS = 'CACSmenos400'
+
+ORG_BASE = 'data/originals/sc/'
+ORG_MAS = os.path.join(ORG_BASE, OUTPUT_MAS)
+ORG_MENOS = os.path.join(ORG_BASE, OUTPUT_MENOS)
+
+FOLDS_BASE = 'data/folds/'
+FOLD_ID = 'fold_'
+
+RUN_BASE = 'data/sc_run'
+
+class ScoreCalciumSelection:
+
+    def __init__(self, criteria='5-fold'):
+        """
+
+        :param criteria:
+        """
+        logging.info(f'Running experiment based on {criteria} cross validation criteria')
+
+        self._criteria = criteria
+        self._nfolds = 5 if criteria=='5-fold' else 10
+
+        fold_length = int(len(os.listdir(ORG_MENOS)) / self._nfolds)
+
+        # self.create_folds(fold_length, self._nfolds)
+
+    def generate_run_set(self, test_fold_id=1):
+
+        if os.path.isdir(RUN_BASE):
+            shutil.rmtree(RUN_BASE)
+        os.mkdir(RUN_BASE)
+        os.mkdir(os.path.join(RUN_BASE, 'train'))
+        os.mkdir(os.path.join(RUN_BASE, 'train', OUTPUT_MAS))
+        os.mkdir(os.path.join(RUN_BASE, 'train', OUTPUT_MENOS))
+        os.mkdir(os.path.join(RUN_BASE, 'test'))
+        os.mkdir(os.path.join(RUN_BASE, 'test', OUTPUT_MAS))
+        os.mkdir(os.path.join(RUN_BASE, 'test', OUTPUT_MENOS))
+
+        for fold_id in range(0, self._nfolds):
+            org_mas = os.path.join(FOLDS_BASE, FOLD_ID + str(fold_id + 1), OUTPUT_MAS)
+            org_menos = os.path.join(FOLDS_BASE, FOLD_ID + str(fold_id + 1), OUTPUT_MENOS)
+            if fold_id + 1 == test_fold_id:
+                # Copy test fold
+                dst_mas = os.path.join(RUN_BASE, 'test', OUTPUT_MAS)
+                dst_menos = os.path.join(RUN_BASE, 'test', OUTPUT_MENOS)
+            else:
+                # Rest of train folds
+                dst_mas = os.path.join(RUN_BASE, 'train', OUTPUT_MAS)
+                dst_menos = os.path.join(RUN_BASE, 'train', OUTPUT_MENOS)
+            copy_tree(org_mas, dst_mas)
+            copy_tree(org_menos, dst_menos)
+
+
+
+    # def create_folds(self, length, nfolds):
+    #     # Create root sc_folds. Remove if exists
+    #     if os.path.isdir(FOLD_ROOT):
+    #         shutil.rmtree(FOLD_ROOT)
+    #     os.mkdir(FOLD_ROOT)
+    #
+    #     for fold in range(nfolds):
+    #         # create fold folder
+    #         os.mkdir(os.path.join(FOLD_ROOT,'fold_' + str(fold + 1)))
+    #
+    #         for img in os.listdir(ORG_MENOS):
+    #             print(img)
+
+
+
+
+
+
---
+++ hyperparams.py
@@ -0,0 +1,12 @@
+import os
+
+# PARAMETERS
+EPOCHS = 1
+BATCH_SIZE = 8
+LEARNING_RATE = 0.0001
+
+# PATHS
+BASE_DATA = 'data/'
+SCORE_CALCIUM_DATA = os.path.join(BASE_DATA, 'sc_run')
+TRAIN = 'train'
+TEST = 'test'
